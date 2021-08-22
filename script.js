ed57f43d8814bb7ae88f3e0e315ee0440bfc9405
Afiq Aiman <73816876+afiqaimanafr@users.noreply.github.com>
2021-08-22 13:25:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -49,27 +49,51 @@ $(document).ready(function(){
     });
 
     // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+    // $('.carousel').owlCarousel({
+    //     margin: 20,
+    //     loop: true,
+    //     autoplay: true,
+    //     autoplayTimeOut: 2000,
+    //     autoplayHoverPause: true,
+    //     responsive: {
+    //         0:{
+    //             items: 1,
+    //             nav: false
+    //         },
+    //         600:{
+    //             items: 2,
+    //             nav: false
+    //         },
+    //         1000:{
+    //             items: 3,
+    //             nav: false
+    //         }
+    //     }
+    // });
+
+    // var form = document.getElementById("my-form");
+    //
+    //     async function handleSubmit(event) {
+    //       event.preventDefault();
+    //       var status = document.getElementById("status");
+    //       var data = new FormData(event.target);
+    //       fetch(event.target.action, {
+    //         method: form.method,
+    //         body: data,
+    //         headers: {
+    //             'Accept': 'application/json'
+    //         }
+    //       }).then(response => {
+    //         status.classList.add('success')
+    //         status.innerHTML = "Thanks for your submission!";
+    //         form.reset()
+    //       }).catch(error => {
+    //         status.classList.add('error')
+    //         status.innerHTML = "Oops! There was a problem submitting your form"
+    //       });
+    //     }
+    //     form.addEventListener("submit", handleSubmit)
+    // });
 });
 
 // var form = document.getElementById("my-form");
